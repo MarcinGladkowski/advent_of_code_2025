201ff4484b55d016f6c62fa111682eacd8ccd898
MarcinGladkowski <marcingladkowski@gmail.com>
2025-12-02 16:07:46 +0100

    day1: first_part

--- day_1/1/main.py
+++ day_1/1/main.py
@@ -3,7 +3,9 @@ def load_test_data() -> list[str]:
     with open("test.txt") as f:
         return list(map(lambda x: x.replace("\n", ""), f.readlines()))
 
-
+def load_data() -> list[str]:
+    with open("input.txt") as f:
+        return list(map(lambda x: x.replace("\n", ""), f.readlines()))
 
 STARTING_POSITION_CONST = 50
 LEFT_DIRECTION_CONST = "L"  
@@ -30,7 +32,11 @@ def move_right(curent_possition_count: int, move_count: int) -> int:
     return curent_possition_count + move_count
 
 
-def run(moves: list[str]) -> int:
+def count_ticks_at_zero(current_position: int) -> int:
+    return 1 if current_position % 100 == 0 else 0
+
+
+def run(moves: list[str], counter_function: callable) -> int:
     current_position = STARTING_POSITION_CONST
     zero_counter = 0
     
@@ -42,11 +48,12 @@ def run(moves: list[str]) -> int:
             current_position = move_left(current_position, count)
         else:
             current_position = move_right(current_position, count)
-            
-        print(current_position)
-            
-        if current_position == 0 or current_position == 100:
-            zero_counter += 1
+        
+        zero_counter += counter_function(current_position)
         
     return zero_counter
+
+
+def count_all_points_zero(moves: list[str]) -> int:
+    return run(moves)
 
--- day_1/1/test_main.py
+++ day_1/1/test_main.py
@@ -21,5 +21,9 @@ def test_right_move():
     assert move_right(82, 30) == 12
     
 def test_result_for_test_data():
-    assert run(load_test_data()) == 3
+    assert run(load_test_data(), count_ticks_at_zero) == 3
+    
+def test_result_for_input_data():
+    print(f"Result for data: {run(load_data()), count_ticks_at_zero}")
+    assert run(load_data()) == 1191
     
