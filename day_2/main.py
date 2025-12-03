2da1cab3cfdc03eb6e883d56fd81de09e8bc25a6
MarcinGladkowski <marcingladkowski@gmail.com>
2025-12-03 20:55:02 +0100

    day_2: part 2 - resolved

--- day_2/main.py
+++ day_2/main.py
@@ -1,3 +1,5 @@
+import re
+
 def load_data(file: str) -> list[str]:
     with open(file) as f:
         return f.readline().strip().split(',')
@@ -16,19 +18,20 @@ def find_invalid_repeated_ids(id_range: tuple[int]):
         
     return invalid_ids
 
-def find_repeat_pattern(value: str) -> bool:
+def find_repeat_pattern(value: str) -> bool:    
+    for i in range(1, len(value) + 1):
+        pattern = value[:i]
+        result = re.findall(pattern, value)
+        
+        if len(result) >= 2 and ''.join(result) == value:
+            return True
+    
     return False
 
 def find_invalid_repeated_ids_more_than_twice(id_range: tuple[int]):
     invalid_ids = []
     for range_number in range(id_range[0], id_range[1] + 1):
-        
-        value = str(range_number)
-        
-        first_half = value[:len(value)//2]
-        second_half = value[len(value)//2:]
-        
-        if first_half == second_half:
+        if find_repeat_pattern(str(range_number)):
             invalid_ids.append(range_number)
         
     return invalid_ids
@@ -36,7 +39,12 @@ def find_invalid_repeated_ids_more_than_twice(id_range: tuple[int]):
 
 def calculate_invalid_ids(source_file: str) -> int:    
     number_ranges = list(map(lambda x: (int(x.split('-')[0]), int(x.split('-')[1])), load_data(source_file)))
-    
     results = [sum(find_invalid_repeated_ids(x)) for x in number_ranges]
+    
+    return sum(results)
+
+def calculate_invalid_repeated_ids(source_file: str) -> int:    
+    number_ranges = list(map(lambda x: (int(x.split('-')[0]), int(x.split('-')[1])), load_data(source_file)))
+    results = [sum(find_invalid_repeated_ids_more_than_twice(x)) for x in number_ranges]
     
     return sum(results)
--- day_2/test_main.py
+++ day_2/test_main.py
@@ -1,4 +1,4 @@
-from main import find_invalid_repeated_ids, calculate_invalid_ids, find_repeat_pattern
+from main import *
 
 def test_invalid_repeated_ids():
     assert find_invalid_repeated_ids([11, 22]) == [11, 22]
@@ -10,6 +10,19 @@ def test_invalid_repeated_ids():
     assert find_invalid_repeated_ids([446443, 446449]) == [446446]
     assert find_invalid_repeated_ids([38593856, 38593862]) == [38593859]
     
+def test_invalid_at_least_twice():
+    assert find_invalid_repeated_ids_more_than_twice([11, 22]) == [11, 22]
+    assert find_invalid_repeated_ids_more_than_twice([95, 115]) == [99, 111]
+    assert find_invalid_repeated_ids_more_than_twice([998, 1012]) == [999, 1010]
+    assert find_invalid_repeated_ids_more_than_twice([1188511880, 1188511890]) == [1188511885]
+    assert find_invalid_repeated_ids_more_than_twice([222220, 222224]) == [222222]
+    assert find_invalid_repeated_ids_more_than_twice([1698522, 1698528]) == []
+    assert find_invalid_repeated_ids_more_than_twice([446443, 446449]) == [446446]
+    assert find_invalid_repeated_ids_more_than_twice([38593856, 38593862]) == [38593859]
+    assert find_invalid_repeated_ids_more_than_twice([565653, 565659]) == [565656]
+    assert find_invalid_repeated_ids_more_than_twice([824824821, 824824827]) == [824824824]
+    assert find_invalid_repeated_ids_more_than_twice([2121212118, 2121212124]) == [2121212121]
+    
 def test_invalid_find_pattern():
     assert find_repeat_pattern('1212121212') == True
     
@@ -18,3 +31,9 @@ def test_sum_invalid_ids_for_test_data():
     
 def test_sum_invalid_ids_for_input_data():
     assert calculate_invalid_ids('day_2/input.txt') == 18595663903
+    
+def test_sum_invalid_repeated_ids_for_test_data():
+    assert calculate_invalid_repeated_ids('day_2/test_input.txt') == 4174379265
+    
+    
+print(calculate_invalid_repeated_ids('day_2/input.txt'))
