4a01fab76b5474e3af79031a208ca5b763cdbe7f
MarcinGladkowski <marcingladkowski@gmail.com>
2025-12-03 23:22:47 +0100

    day_3: part two - working on optimization combinations generation

--- day_3/main.py
+++ day_3/main.py
@@ -1,8 +1,17 @@
 from itertools import combinations
 
-def get_largest_voltage(value: str):
-    return str(max(list(map(lambda x: int(x[0] + x[1]), combinations(value, 2)))))
+def get_largest_voltage(value: str, factor: int = 2):
+    
+    max_combination = 0
+    combinations_list = combinations(value, factor)
+    
+    for combo in combinations_list:
+        combination_value = int(''.join(combo))
+        if combination_value > max_combination:
+            max_combination = combination_value
+    
+    return str(max_combination)
 
-def calculate_max_voltage_sum(values: list[str]) -> int:
-    return sum(map(lambda x: int(get_largest_voltage(x)), values))
+def calculate_max_voltage_sum(values: list[str], factor: int = 2) -> int:
+    return sum(map(lambda x: int(get_largest_voltage(x, factor)), values))
     
--- day_3/test_main.py
+++ day_3/test_main.py
@@ -13,3 +13,16 @@ def test_calculate_max_voltage_sum():
 
 def test_calculate_max_voltage_sum_for_first_challenge():
     assert calculate_max_voltage_sum(load_data("day_3/input.txt")) == 17142
+    
+def test_get_largest_voltage_for_factor_twelve():
+    assert get_largest_voltage("987654321111111", 12) == '987654321111'
+    assert get_largest_voltage("234234234234278", 12) == '434234234278'
+    
+  
+print(calculate_max_voltage_sum(['2252522232121122125212322341424262435212421332333533223124122242222222112222222222423222112211212132'], 12))    
+# full_data = load_data("day_3/input.txt")
+# total = 0
+# n = 1
+# for i in range(0, len(full_data)+1, n):
+#     total += calculate_max_voltage_sum(full_data[i:i+n], 12)
+#     print(total)
