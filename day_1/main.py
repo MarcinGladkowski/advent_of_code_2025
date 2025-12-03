16559dff1716b56f1dbc047e458e66035264a971
MarcinGladkowski <marcingladkowski@gmail.com>
2025-12-03 06:51:14 +0100

    ref: return full dial circles

--- day_1/main.py
+++ day_1/main.py
@@ -12,26 +12,37 @@ def get_move_direction(instruction: str) -> str:
 def get_move_count(instruction: str) -> int:
     return int(instruction[1:])
 
-def move_left(curent_possition_count: int, move_count: int) -> int:
+def move_left(curent_possition_count: int, move_count: int) -> tuple[int, int]  :
     
     if curent_possition_count - move_count < 0:
-        return 100 + (curent_possition_count - move_count)
+        
+        full_circles = move_count // 100 if move_count % 100 == 0 else 1
+        
+        return 100 * full_circles + (curent_possition_count - move_count), full_circles
     
-    return curent_possition_count - move_count
+    full_dial_circles = 1
+    
+    return curent_possition_count - move_count, full_dial_circles
 
-def move_right(curent_possition_count: int, move_count: int) -> int:
+def move_right(curent_possition_count: int, move_count: int) -> tuple[int, int]:
     
     if curent_possition_count + move_count > 100:
 
         full_circles = move_count // 100 if move_count % 100 == 0 else 1
         
-        return (curent_possition_count + move_count) - full_circles * 100
+        return (curent_possition_count + move_count) - full_circles * 100, full_circles
     
-    return curent_possition_count + move_count
+    full_dial_circles = 1
+    
+    return curent_possition_count + move_count, full_dial_circles
 
 
-def count_ticks_at_zero(current_position: int) -> int:
+def count_ticks_at_zero(current_position: int, full_dial_circles: int) -> int:
     return 1 if current_position % 100 == 0 else 0
+
+
+def count_all_dial_points(current_position: int, full_dial_circles: int) -> int:
+    return full_dial_circles
 
 
 def run(moves: list[str], counter_function: callable) -> int:
@@ -43,14 +54,14 @@ def run(moves: list[str], counter_function: callable) -> int:
         count = get_move_count(move)
         
         if direction == LEFT_DIRECTION_CONST:
-            current_position = move_left(current_position, count)
+            current_position, full_dial_circles = move_left(current_position, count)
         else:
-            current_position = move_right(current_position, count)
+            current_position, full_dial_circles = move_right(current_position, count)
         
         
-        print(f"\nMove: {move}, Current position: {current_position}")
+        #print(f"\nMove: {move}, Current position: {current_position}")
     
-        zero_counter += counter_function(current_position)
+        zero_counter += counter_function(current_position, full_dial_circles)
         
     return zero_counter
 
--- day_1/test_main.py
+++ day_1/test_main.py
@@ -9,26 +9,22 @@ def test_get_move_count():
     assert get_move_count("R5") == 5
     
 def test_left_move():
-    assert move_left(50, 68) == 82
-    assert move_left(82, 30) == 52
-    
-def test_left_move():
-    assert move_left(50, 68) == 82
-    assert move_left(82, 30) == 52
-    
-def test_right_move():
-    assert move_right(50, 68) == 18
-    assert move_right(82, 30) == 12
-    assert move_right(50, 1000) == 50
-    
-# def test_result_for_R1000_after_start():    
-#     assert run(['R1000'], count_ticks_at_zero) == 10
+    assert move_left(50, 68)[0] == 82
+    assert move_left(82, 30)[0] == 52
 
-# def test_result_for_test_data():
-#     assert run(load_data("test.txt"), count_ticks_at_zero) == 3
+def test_right_move():
+    assert move_right(50, 68)[0] == 18
+    assert move_right(82, 30)[0] == 12
+    assert move_right(50, 1000)[0] == 50
+        
+def test_result_for_R1000_after_start():    
+    assert run(['R1000'], count_all_dial_points) == 10
+
+def test_result_for_test_data():
+    assert run(load_data("test.txt"), count_ticks_at_zero) == 3
     
-# def test_result_for_input_data():
-#     assert run(load_data("input.txt"), count_ticks_at_zero) == 1191
+def test_result_for_input_data():
+    assert run(load_data("input.txt"), count_ticks_at_zero) == 1191
     
 # def test_count_all_dial_points_for_test_data():
 #     assert all_dial_points(load_data("test.txt")) == 6
